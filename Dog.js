b266aa0ef53ba2509919f6fe30ad790f8fd38557
Meobot <nathanrhodes312@gmail.com>
2022-07-20 22:20:06 -0400

    framework of project complete

---
+++ Dog.js
@@ -0,0 +1,15 @@
+import dogs from './data.js'
+
+//TODO getstamp function
+//TODO generate dog profile function
+
+class Dog {
+    constructor() {
+        Object.assign(this, data)
+
+        //properties
+    }
+    // methods out here
+
+}
+
---
+++ index.js
@@ -0,0 +1,4 @@
+//TODO like function
+//TODO dislike function
+//TODO get new dog function
+//TODO rendering function
